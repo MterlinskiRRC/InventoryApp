5ce81231c4d43f0f50aa6f4945106c722b0e0846
“Matthew <Mterlinski@rrc.ca>
2026-03-26 13:14:17 -0500

    Simplify delete operation - use item data directly without type conversion

--- lambda/delete_inventory_item/lambda_function.py
+++ lambda/delete_inventory_item/lambda_function.py
@@ -1,6 +1,5 @@
 import boto3
 import json
-from decimal import Decimal
 from boto3.dynamodb.conditions import Key
 
 def lambda_handler(event, context):
@@ -46,8 +45,8 @@ def lambda_handler(event, context):
 
         table.delete_item(
             Key={
-                'item_id': str(item['item_id']),
-                'item_location_id': Decimal(str(item['item_location_id']))
+                'item_id': item['item_id'],
+                'item_location_id': item['item_location_id']
             }
         )
 
